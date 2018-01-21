4088e60991afa791539741c28d826e1eb891d1cf
avastmick <avastmick@outlook.com>
2018-01-21 15:37:55 +0800

    Defined project as a Rust Workspace so the sub-dirs can be hooked together

--- cipher-one/src/main.rs
+++ cipher-one/src/main.rs
@@ -1,132 +1,6 @@
-#[macro_use] extern crate maplit; 
-#[macro_use] extern crate lazy_static;
-
 use std::env;
-use std::collections::HashMap;
+extern crate cipher_one;
 
-const CODE_LEN: usize = 4;
-
-lazy_static! {
-    static ref CODES: HashMap<&'static str, &'static str> = hashmap!{
-        "a" => "6520",
-        "b" => "2143",
-        "c" => "3990",
-        "d" => "9533",
-        "e" => "1249",
-        "f" => "8942",
-        "g" => "1043",
-        "h" => "1148",
-        "i" => "2397",
-        "j" => "7753",
-        "k" => "6521",
-        "l" => "6780",
-        "m" => "0067",
-        "n" => "1258",
-        "o" => "5698",
-        "p" => "9901",
-        "q" => "9806",
-        "r" => "6683",
-        "s" => "6799",
-        "t" => "5320",
-        "u" => "3118",
-        "v" => "2679",
-        "w" => "1069",
-        "x" => "9001",
-        "y" => "5477",
-        "z" => "9900"
-    };
-}
-
-/// Gets the key (the source character) for a given cipher text code
-fn get_key(code: &String) -> String {
-    let mut key = String::new();
-
-    for (_key, val) in CODES.iter() {
-        if val == &code {
-            key.push_str(_key);
-        }
-    }
-    key
-}
-
-/// Get the code for a given key (source character)
-fn get_code(key: String) -> String {
-    let mut code = String::new();
-    if CODES.contains_key(key.to_lowercase().as_str()) {
-        code.push_str(CODES.get(key.to_lowercase().as_str()).unwrap() );
-    }
-    code
-}
-
-/// Encode the given message
-///
-/// assert_eq!("11481249678067805698 10695698668367809533", encode("Hello World"));
-fn encode(msg: &str) -> String {
-
-    let mut secret = String::new();
-    // Iterate through the message
-    for c in msg.chars() { 
-        if c == ' ' {
-            secret.push(c);
-        } else { // get code and add to secret
-            let mut key = String::new();
-            key.push(c);
-            secret += &get_code(key);
-        }
-    }
-    if secret.trim() == "" {
-        secret += "Encoding failed! Please use on alphabetical values!";
-    }
-    secret
-}
-
-/// Decode the given message
-///
-/// assert_eq!("hello world", decode("11481249678067805698 10695698668367809533"));
-fn decode(secret: &str) -> String {
-    
-    let mut msg = String::new();
-    let mut code = String::new();
-    println!("
-    Got secret: {}", secret);
-    for c in secret.chars() {
-
-        if c != ' ' {
-            code.push(c);
-            // If we have the right length code
-            if code.len() == CODE_LEN {
-                // Look up the key from value
-                msg += &get_key(&code);
-                // Reset
-                code.clear();
-            }
-        } else { // Ignore from code, but add to message
-            msg.push(c);
-            // Reset
-            code.clear();
-        }
-    }
-    if msg.trim() == "" {
-        msg += "Nothing to decode. Bad code!";
-    }
-    msg
-}
-
-fn usage() {
-    println!("
-    **********************************************************
-    Usage: cipher-one [-d] message
-        
-            to decode, use \'-d\'
-    **********************************************************");
-}
-
-fn print_out(msg: String) {
-    println!("
-    **********************************************************
-    {}
-    **********************************************************", msg);
-}
 
 //  Fails on reading args from commandline...
 fn main() {
@@ -135,28 +9,15 @@ fn main() {
     if args.len() == 3 { // Should be looking to decode
         let message = &args[2];
         if args[1] == "decode" { 
-            print_out(decode(message.as_str()));
+            cipher_one::print_out(cipher_one::decode(message.as_str()));
         }
         else { // Print usage message
-            usage();
+            cipher_one::usage();
         }
     } else if args.len() ==2 {
         let message = &args[1];
-        print_out(encode(message.as_str()));
+        cipher_one::print_out(cipher_one::encode(message.as_str()));
     } else { // Print usage message
-        usage();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_encode() {
-        assert_eq!("11481249678067805698 10695698668367809533", encode("Hello World"));
-    }
-    #[test]
-    fn test_decode() {
-        assert_eq!("hello world", decode("11481249678067805698 10695698668367809533"));
+        cipher_one::usage();
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate cipher_one;
+
+fn main() {
+
+    println!("Encoding \"Hello, world!\" gives: ");
+    cipher_one::print_out(cipher_one::encode("Hello World!"));
+}
+
+

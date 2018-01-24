af3e2c330c4e55c26f2ed2e70d17bf248ffe0b79
avastmick <avastmick@outlook.com>
2018-01-24 16:04:11 +0800

    Reset the root crate to run against cipher-one

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-extern crate cipher_two;
+extern crate cipher_one;
 
 fn main() {
 
     println!("Encoding \"Hello, world!\" gives: ");
-    cipher_two::print_out(cipher_two::encode("Hello World!"));
+    cipher_one::print_out(cipher_one::encode("Hello World!"));
 }
 
 
